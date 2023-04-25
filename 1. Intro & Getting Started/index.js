caca085424d9e7b786a966571ed925f9f9af32ee
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2023-04-25 15:46:11 +0500

    feat:readME

---
+++ 1. Intro & Getting Started/index.js
@@ -0,0 +1,22 @@
+/*
+What is JavaScript ? 🤔🤔🤔
+
+JavaScript -- is one of the core technologies of the web, alongside HTML & CSS. 
+It is a high level, interpreted programming language that can be used on the client side as
+well as the server-side with Node.js
+
+*/
+
+/* 
+What is JavaScript Used For ? 🤔🤔🤔
+
+DOM - (Document Object Model) Manipulation
+Event Handling
+Asynchronous Requests
+Animations & Effects
+Data Manipulation (Sorting, filtering, etc)
+Storing Data (Cookies, LocalStorage, etc)
+Single Page Application (SPA)
+Creating APIs & Web Services (Node.js, Deno)
+
+*/
