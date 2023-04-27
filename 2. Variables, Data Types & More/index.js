3aff93b4de1abcfe8acdbc4effa7a603103dd06c
shoxruxshomurodov <shomurodovshoxrux1998@gmail.com>
2023-04-27 10:59:50 +0500

    feat:variablesandDataTypes

---
+++ 2. Variables, Data Types & More/index.js
@@ -0,0 +1,26 @@
+/* Variables, Data Types & More 🤔🤔🤔 */
+
+// alert("Hello JavaScript");
+
+// Console methods
+console.group("Simple");
+console.log(100);
+console.log("Hello JavaScript");
+console.log(24, "Hello", true);
+console.groupEnd();
+const x = 100;
+console.log(x);
+
+console.error("This is error message");
+console.warn("Warning");
+console.table([{ name: "Shoxrux", email: "shoxruxshomurodoff@gmail.com" }]);
+
+const styles = "padding: 10px; background-color: gray; color: green";
+console.log("%cAssalomu alaykum Shoxrux aka", styles);
+
+// This is a single line of comment
+
+/* This is a multiple
+ line of comment */
+
+/* Variables & Constants 🤔🤔🤔 */
